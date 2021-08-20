5b476667a9f4139658f38ccf65d926f82af0ba8b
Kiran B A <kiran.b.a@sap.com>
2021-08-20 04:14:39 +0000

    table component created

--- assignmentrepo/webapp/controller/App.controller.js
+++ assignmentrepo/webapp/controller/App.controller.js
@@ -1,15 +1,20 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter"
 ],
-	/**
-	 * @param {typeof sap.ui.core.mvc.Controller} Controller
-	 */
-	function (Controller) {
-		"use strict";
-
-		return Controller.extend("assignmentrepo.controller.App", {
-			onInit: function () {
-
-			}
-		});
-	});
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, Fragment, JSONModel, Filter) {
+        "use strict";
+ 
+        return Controller.extend("assignmentrepo.controller.View1", {
+            onInit: function () {
+                $.get("model/game.json").then((oModel)=>{
+                    this.getView().setModel(new JSONModel(oModel),"games");
+                });
+            }
+        });
+    });
